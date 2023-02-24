14e494d6745474ce704ff92314d21e6071bde0c3
alvaroRepRom <104201349+alvaroRepRom@users.noreply.github.com>
2023-02-24 11:51:14 +0100

    InputKey Rebind

--- Assets/Scripts/UI/OptionsUI.cs
+++ Assets/Scripts/UI/OptionsUI.cs
@@ -9,8 +9,23 @@ public class OptionsUI : MonoBehaviour
     [SerializeField] private Button sfxButton;
     [SerializeField] private Button musicButton;
     [SerializeField] private Button closeButton;
+    [SerializeField] private Button moveUpButton;
+    [SerializeField] private Button moveDownButton;
+    [SerializeField] private Button moveLeftButton;
+    [SerializeField] private Button moveRightButton;
+    [SerializeField] private Button interactButton;
+    [SerializeField] private Button interactAlternateButton;
+    [SerializeField] private Button pauseButton;
     [SerializeField] private TextMeshProUGUI sfxText;
     [SerializeField] private TextMeshProUGUI musicText;
+    [SerializeField] private TextMeshProUGUI moveUpText;
+    [SerializeField] private TextMeshProUGUI moveDownText;
+    [SerializeField] private TextMeshProUGUI moveLeftText;
+    [SerializeField] private TextMeshProUGUI moveRightText;
+    [SerializeField] private TextMeshProUGUI interactText;
+    [SerializeField] private TextMeshProUGUI interactAlternateText;
+    [SerializeField] private TextMeshProUGUI pauseText;
+    [SerializeField] private Transform pressToRebingTrans;
 
     private void Awake()
     {
@@ -32,6 +47,14 @@ public class OptionsUI : MonoBehaviour
         {
             Hide();
         } );
+        
+        moveUpButton.onClick.AddListener( () => { RebindKey( GameInput.Binding.Move_Up ); } );
+        moveDownButton.onClick.AddListener( () => { RebindKey( GameInput.Binding.Move_Down ); } );
+        moveLeftButton.onClick.AddListener( () => { RebindKey( GameInput.Binding.Move_Left ); } );
+        moveRightButton.onClick.AddListener( () => { RebindKey( GameInput.Binding.Move_Right ); } );
+        interactButton.onClick.AddListener( () => { RebindKey( GameInput.Binding.Interact ); } );
+        interactAlternateButton.onClick.AddListener( () => { RebindKey( GameInput.Binding.Interact_Alternate ); } );
+        pauseButton.onClick.AddListener( () => { RebindKey( GameInput.Binding.Pause ); } );
     }
 
     private void Start()
@@ -39,6 +62,7 @@ public class OptionsUI : MonoBehaviour
         KitchenGameManager.Instance.OnGameUnPaused += KitchenGameManager_OnGameUnPaused;
 
         UpdateVisuals();
+        HidePressToRibingKey();
         Hide();
     }
 
@@ -51,6 +75,14 @@ public class OptionsUI : MonoBehaviour
     {
         sfxText.text = "Sound Effects: " + Mathf.Round( SoundManager.Instance.GetVolume() * 10f );
         musicText.text = "Music: " + Mathf.Round( MusicManager.Instance.GetVolume() * 10f );
+
+        moveUpText.text = GameInput.Instance.GetBindingText( GameInput.Binding.Move_Up );
+        moveDownText.text = GameInput.Instance.GetBindingText( GameInput.Binding.Move_Down );
+        moveLeftText.text = GameInput.Instance.GetBindingText( GameInput.Binding.Move_Left );
+        moveRightText.text = GameInput.Instance.GetBindingText( GameInput.Binding.Move_Right );
+        interactText.text = GameInput.Instance.GetBindingText( GameInput.Binding.Interact );
+        interactAlternateText.text = GameInput.Instance.GetBindingText( GameInput.Binding.Interact_Alternate );
+        pauseText.text = GameInput.Instance.GetBindingText( GameInput.Binding.Pause );
     }
 
     public void Show()
@@ -61,5 +93,25 @@ public class OptionsUI : MonoBehaviour
     private void Hide()
     {
         gameObject.SetActive( false );
+    }
+
+    private void ShowPressToRibingKey()
+    {
+        pressToRebingTrans.gameObject.SetActive( true );
+    }
+    
+    private void HidePressToRibingKey()
+    {
+        pressToRebingTrans.gameObject.SetActive( false );
+    }
+
+    private void RebindKey( GameInput.Binding binding )
+    {
+        ShowPressToRibingKey();
+        GameInput.Instance.RebindBinding( binding, () =>
+        {
+            HidePressToRibingKey();
+            UpdateVisuals();
+        } );
     }
 }
